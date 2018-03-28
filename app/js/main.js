c680386c625b40405134b00aba2e239dd50f4543
Taufan Novembri <taufanovembri@Taufans-MBP.ti.gramedia.net>
2018-03-28 17:47:13 +0700

    Revision 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 console.log('Welcome to Task!');
 
 $(document).scroll(function(){
+    "use strict";
     $('.block_video, .caption-video, .block_progress').toggleClass('scroll', $(this).scrollTop() > 600);
     $('.caption-video span').click(function() {
         $(this).parentsUntil( $( '.block' ) ).addClass('hide');
@@ -20,6 +21,7 @@ $(document).scroll(function(){
 });
 
 $(function () {
+    "use strict";
     $('a[href="#search"]').on('click', function(event) {
         event.preventDefault();
         $('#search').addClass('open');
@@ -27,19 +29,63 @@ $(function () {
     });
     
     $('#search, #search button.close').on('click keyup', function(event) {
-        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+        if (event.target === this || event.target.className === 'close' || event.keyCode === 27) {
             $(this).removeClass('open');
         }
     });
 });
 
 // When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+
 
 function myFunction() {
+    "use strict";
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 
+window.onscroll = function() {"use strict"; myFunction();};
+
+
+//youtube script
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+onYouTubeIframeAPIReady = function () {
+    player = new YT.Player('player', {
+        videoId: 'Nsec4hWZz2M',  // youtube video id
+        playerVars: {
+            'autoplay': 0,
+            'rel': 0,
+            'showinfo': 0
+        },
+        events: {
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+var p = document.getElementById ("player");
+$(p).hide();
+
+var t = document.getElementById ("thumbnail");
+t.src = "http://img.youtube.com/vi/Nsec4hWZz2M/maxresdefault.jpg";
+
+onPlayerStateChange = function (event) {
+    if (event.data == YT.PlayerState.ENDED) {
+        $('.start-video').fadeIn('normal');
+    }
+}
+
+$(document).on('click', '.start-video', function () {
+    $(this).hide();
+    $("#player").show();
+    $("#thumbnail_container").hide();
+    player.playVideo();
+});
